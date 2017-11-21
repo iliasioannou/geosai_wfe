03b54fc0818b6d301d295364cac00680ada0ee73
Ilias Ioannou <ioannou@planetek.gr>
2017-11-21 17:09:41 +0100

    Updated build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -t planetek/cmems_activiti:base -f Dockerfile.base .
-docker build --no-cache -t planetek/cmems_activiti:$1 --build-arg branch=$1 .
+docker build -t planetek/eosai_activiti:base -f Dockerfile.base .
+docker build --no-cache -t planetek/eosai_activiti:$1 --build-arg branch=$1 .
